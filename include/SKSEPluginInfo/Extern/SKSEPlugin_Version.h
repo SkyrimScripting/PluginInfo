12240c5c2303ef7b58c7c2ceb1b8068c16815d9e
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-09 12:01:08 -0700

    Use separate constants for AE Version struct

--- include/SKSEPluginInfo/Extern/SKSEPlugin_Version.h
+++ include/SKSEPluginInfo/Extern/SKSEPlugin_Version.h
@@ -2,6 +2,8 @@
 
 // This is only for AE 1.6+ SKSE plugins
 
+// For this include to work, the values must be defined as inline constexpr by the build system
+
 #include <algorithm>
 #include <span>
 
@@ -10,10 +12,10 @@
 extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = []() {
     SKSE::PluginVersionData v{};
 
-    v.PluginVersion(SKSEPluginInfo::PluginVersion);
-    v.PluginName(SKSEPluginInfo::PluginName.data());
-    v.AuthorName(SKSEPluginInfo::AuthorName.data());
-    v.AuthorEmail(SKSEPluginInfo::AuthorEmail.data());
+    v.PluginVersion(SKSEPluginInfoConstants::PluginVersion);
+    v.PluginName(SKSEPluginInfoConstants::PluginName.data());
+    v.AuthorName(SKSEPluginInfoConstants::AuthorName.data());
+    v.AuthorEmail(SKSEPluginInfoConstants::AuthorEmail.data());
 
     v.UsesAddressLibrary();
     v.UsesUpdatedStructs();
