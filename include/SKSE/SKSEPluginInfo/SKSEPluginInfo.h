b735c5c133ae2cc172270eb7a4c56183cfd81d68
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-09 09:40:05 -0700

    Basic interface

---
+++ include/SKSE/SKSEPluginInfo/SKSEPluginInfo.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// These functions need to be defined in your plugin's source file (or by a build system)
+
+#include <REL/Relocation.h>
+
+#include <cstdint>
+#include <string>
+
+namespace SKSEPluginInfo {
+    const char*   GetPluginName();
+    std::uint16_t GetPluginVersionMajor();
+    std::uint16_t GetPluginVersionMinor();
+    std::uint16_t GetPluginVersionPatch();
+
+    std::string GetPluginVersionString() {
+        return std::to_string(GetPluginVersionMajor()) + "." +
+               std::to_string(GetPluginVersionMinor()) + "." +
+               std::to_string(GetPluginVersionPatch());
+    }
+
+    REL::Version GetPluginVersionStruct() {
+        return REL::Version{
+            GetPluginVersionMajor(), GetPluginVersionMinor(), GetPluginVersionPatch()};
+    }
+}
