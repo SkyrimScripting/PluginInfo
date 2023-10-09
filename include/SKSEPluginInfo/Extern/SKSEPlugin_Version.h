52a8a837438a1126ba6dcd132ac13bec4369cbc5
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-09 11:43:09 -0700

    Return correct char[] type

--- include/SKSEPluginInfo/Extern/SKSEPlugin_Version.h
+++ include/SKSEPluginInfo/Extern/SKSEPlugin_Version.h
@@ -10,14 +10,14 @@
 namespace {
     // Lovely hacky fun time to get the constinit working OK for the various string_view fields
 
-    constexpr auto to_char_array_256(const std::string_view sv) -> std::array<char, 256> {
-        std::array<char, 256> arr = {};
-        for (size_t i = 0; i < sv.size() && i < arr.size(); ++i) arr[i] = sv[i];
+    constexpr char[256] to_char_array_256(const std::string_view sv) {
+        char arr[256] = {};
+        for (size_t i = 0; i < sv.size() && i < 256; ++i) arr[i] = sv[i];
         return arr;
     }
-    constexpr auto to_char_array_252(const std::string_view sv) -> std::array<char, 252> {
-        std::array<char, 252> arr = {};
-        for (size_t i = 0; i < sv.size() && i < arr.size(); ++i) arr[i] = sv[i];
+    constexpr char[252] to_char_array_252(const std::string_view sv) {
+        char arr[252] = {};
+        for (size_t i = 0; i < sv.size() && i < 252; ++i) arr[i] = sv[i];
         return arr;
     }
 }
