a35684b4fb825173a19ac40638c738161ce50b3d
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-09 11:45:53 -0700

    Move into namespace to access helpers

--- include/SKSEPluginInfo/Extern/SKSEPlugin_Version.h
+++ include/SKSEPluginInfo/Extern/SKSEPlugin_Version.h
@@ -20,26 +20,21 @@ namespace {
         for (size_t i = 0; i < sv.size() && i < 252; ++i) arr[i] = sv[i];
         return arr;
     }
+
+    extern "C" __declspec(dllexport) constinit SKSE::PluginVersionData SKSEPlugin_Version{
+        .pluginVersion = SKSEPluginInfo::PluginVersion.pack(),
+        .pluginName    = to_char_array_256(SKSEPluginInfo::PluginName),
+        .author        = to_char_array_256(SKSEPluginInfo::AuthorName),
+        .supportEmail  = to_char_array_252(SKSEPluginInfo::AuthorEmail),
+        /*
+            kVersionIndependent_AddressLibraryPostAE = 1 << 0,
+            kVersionIndependent_Signatures = 1 << 1,
+            kVersionIndependent_StructsPost629 = 1 << 2,
+
+            versionIndependency |= kVersionIndependent_AddressLibraryPostAE; (1 << 0)
+            versionIndependency |= kVersionIndependent_StructsPost629; (1 << 2)
+
+            versionIndependency = 5 (1 | 4) [0101 = 5]
+        */
+        .versionIndependence = 5};
 }
-
-// 	constexpr void AuthorEmail(std::string_view a_email) noexcept { SetCharBuffer(a_email,
-// std::span{ supportEmail }); } 		constexpr void AuthorName(std::string_view a_name) noexcept
-// { SetCharBuffer(a_name, std::span{ author }); } constexpr void PluginName(std::string_view
-// a_plugin) noexcept { SetCharBuffer(a_plugin, std::span{ pluginName }); }
-
-extern "C" __declspec(dllexport) constinit SKSE::PluginVersionData SKSEPlugin_Version{
-    .pluginVersion = SKSEPluginInfo::PluginVersion.pack(),
-    .pluginName    = to_char_array_256(SKSEPluginInfo::PluginName),
-    .author        = to_char_array_256(SKSEPluginInfo::AuthorName),
-    .supportEmail  = to_char_array_252(SKSEPluginInfo::AuthorEmail),
-    /*
-        kVersionIndependent_AddressLibraryPostAE = 1 << 0,
-        kVersionIndependent_Signatures = 1 << 1,
-        kVersionIndependent_StructsPost629 = 1 << 2,
-
-        versionIndependency |= kVersionIndependent_AddressLibraryPostAE; (1 << 0)
-        versionIndependency |= kVersionIndependent_StructsPost629; (1 << 2)
-
-        versionIndependency = 5 (1 | 4) [0101 = 5]
-    */
-    .versionIndependence = 5};
